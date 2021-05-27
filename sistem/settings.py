d23836cf3a4d0bc9ccb392c4c294310216ced8f9
KurangKering <cybercature@gmail.com>
2021-05-27 16:27:51 +0700

    postgres

--- sistem/settings.py
+++ sistem/settings.py
@@ -157,6 +157,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 IMPORT_EXPORT_USE_TRANSACTIONS = False
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 django_heroku.settings(locals())
 
 
